f20ab6fcd92833bc3bba670c5ad16b6590cf4f5c
keeley <keeley.jun@qq.com>
2019-09-10 18:47:57 +0800

    sql生成的

--- src/main/java/cc/sitec/generator/GeneratorServiceEntity.java
+++ src/main/java/cc/sitec/generator/GeneratorServiceEntity.java
@@ -6,8 +6,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
-import com.baomidou.mybatisplus.generator.config.rules.FileType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 /**
